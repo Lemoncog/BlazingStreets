02bdc36e92b28e8a9177510ae18ff6d1635fb0c1
Lemoncog <lemoncog@gmail.com>
2014-10-19 00:11:32 +0100

    I forgot because I spent hours trying to make JSHint happy. Given up with JSHint as pre-commit

---
+++ public/js/webapp.js
@@ -0,0 +1,41 @@
+/* global MozActivity, alert, console, Notification */
+(function() {
+  'use strict';
+  /*Map
+   */
+  var map = L.map('map').setView([51.505, -0.09], 13);
+  L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+    attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
+    maxZoom: 18
+  }).addTo(map);
+
+
+  //Get the users GPS
+  var overlayStatus = document.querySelector('#overlayStatus');
+  overlayStatus.innerHTML = 'Searching for location...';
+
+  var id;
+  var options;
+
+  function success(pos) {
+    var crd = pos.coords;
+
+    map.setView([crd.latitude, crd.longitude], 16);
+
+    overlayStatus.innerHTML = 'Good location fix';
+    overlayStatus.style.display = 'block';
+  }
+
+  function error(err) {
+    overlayStatus.innerHTML = 'Cannot get a location fix';
+    overlayStatus.style.display = 'block';
+  }
+
+  options = {
+    enableHighAccuracy: false,
+    timeout: 15000,
+    maximumAge: 0
+  };
+
+  id = navigator.geolocation.watchPosition(success, error, options);
+})();
---
+++ routes/api.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+/* mocked model
+var model = {
+  task: []
+};
+*/
+
+// enable route if need dynamic web
+/*
+// create
+router.post('/', function(req, res) {
+  res.render('index.html', {foo: 'bar'});
+});
+
+// retrieve all
+router.get('/', function(req, res) {
+  res.render('index.html', {foo: 'bar'});
+});
+
+// retrieve
+router.get('/:id?', function(req, res) {
+  res.render('index.html', {foo: 'bar'});
+});
+
+// replace
+router.put('/:id?', function(req, res) {
+  res.render('index.html', {foo: 'bar'});
+});
+
+// update
+router.patch('/:id?', function(req, res) {
+  res.render('index.html', {foo: 'bar'});
+});
+
+// delete
+router.delete('/:id?', function(req, res) {
+  res.render('index.html', {foo: 'bar'});
+});
+*/
+
+module.exports = router;
